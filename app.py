7178adf36ed2d9153a5d2b6b9b0b756d2e956dcb
Sylvester009 <sasy260902@gmail.com>
2024-09-22 21:43:06 +0100

    routes created for member and librarian

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ CORS(app)
 
 @app.route('/')
 def home():
-    return jsonify(message='Welcome to the LMS visitor page(landing page)')
+    return jsonify(message='Welcome to the LMS visitor page!')
+
+@app.route('/member')
+def member():
+    return jsonify(message='Welcome to the LMS member page!')
+
+@app.route('/librarian')
+def librarian():
+    return jsonify(message='Welcome to the LMS librarian page!')
 
 if __name__ == '__main__':
     app.run(debug=True)
